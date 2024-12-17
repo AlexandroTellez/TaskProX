803e840ca59c7373e1e91694c7688fa8534ef661
Alex Tellez <alextellezyanes@gmail.com>
2024-12-17 22:46:03 +0100

    readme and create https requests in main.py

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,26 @@ app = FastAPI()
 @app.get('/')
 def welcome():
     return{'message':'Welcome to the FastAPI API!'}
+
+# Routes with parameters must go first
+@app.get('/api/tasks/{id}')
+async def get_task(id: int): # We declare id as parameter
+    return 'single task'
+
+@app.put('/api/tasks/{id}')
+async def update_task(id: int): # We declare id as parameter
+    return 'updating task'
+
+@app.delete('/api/tasks/{id}')
+async def delete_tasks(id: int): # We declare id as parameter
+    return 'delete task'
+
+# Routes without parameters must go after
+@app.get('/api/tasks')
+async def get_tasks():
+    return 'all tasks'
+
+@app.post('/api/tasks')
+async def create_task():
+    return 'create task'
+
