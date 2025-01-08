22cd24f66a8fb56181c72b6f25b1b50d7c77fe9b
Alex Tellez <alextellezyanes@gmail.com>
2025-01-08 22:32:28 +0100

    create home page y task form route and install tailwind

---
+++ client/src/pages/TaskForm.jsx
@@ -0,0 +1,16 @@
+/**
+ * TaskForm component that renders the Task Form content.
+ *
+ * @component
+ * @example
+ * return (
+ *   <TaskForm />
+ * )
+ */
+function TaskForm(){
+    return (
+        <div>TaskForm</div>
+    )
+}
+
+export default TaskForm
